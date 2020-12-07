4de60b72216d1fd8fbe4593722fc4b13a3f52a94
maleksejev <>
2020-12-07 18:56:00 +0200

    With changes for ex48_1 v2

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,4 @@
 class Question < ApplicationRecord
-
-  belongs_to :user
   validates :text, :user, presence: true
   validates_length_of :text, maximum: 255
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,34 +1,28 @@
 require 'openssl'
 
 class User < ApplicationRecord
-
-  ITERATIONS = 20_000
   DIGEST = OpenSSL::Digest::SHA256.new
-
+  ITERATIONS = 20_000
+  USERNAME_FORMAT = /\A\w+\z/
+  attr_accessor :password
   has_many :questions
-  validates :email, :username, presence: true
-  validates :email, :username, uniqueness: true
+  validates :email, :username, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates_length_of :username, minimum: 5, maximum: 40
-  validates :username, format: { with: /\A[a-zA-Z0-9_]+\z/, message: "Only letters, digits and '_' " }
-
-  attr_accessor :password
-
+  validates :username, format: { with: USERNAME_FORMAT }
   validates_presence_of :password, on: :create
   validates_confirmation_of :password
-
   before_save :encrypt_password
 
   def encrypt_password
-      if password.present?
+    if password.present?
       self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
       self.password_hash = User.hash_to_string(
-        OpenSSL::PKCS5.pbkdf2_hmac(
-          password, password_salt, ITERATIONS, DIGEST.length, DIGEST
-        )
-      )
+      OpenSSL::PKCS5.pbkdf2_hmac(password, password_salt, ITERATIONS, DIGEST.length, DIGEST))
     end
   end
+
+  private
 
   def self.hash_to_string(password_hash)
     password_hash.unpack('H*')[0]
@@ -37,12 +31,8 @@ class User < ApplicationRecord
   def self.authenticate(email, password)
     user = find_by(email: email)
     return nil unless user.present?
-
     hashed_password = User.hash_to_string(
-      OpenSSL::PKCS5.pbkdf2_hmac(
-        password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST
-      )
-    )
+    OpenSSL::PKCS5.pbkdf2_hmac(password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST))
     return user if user.password_hash == hashed_password
     nil
   end
