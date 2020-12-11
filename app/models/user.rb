ab5a846db4913b542f1c5daab998e929f2beca56
maleksejev <>
2020-12-11 16:00:20 +0200

    Downcase username 48_1_2 v4 exercises

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,17 +21,7 @@ class User < ApplicationRecord
   def downcase_username
     self.username.downcase!
   end
-
-  private
-
-  def encrypt_password
-    if password.present?
-      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
-      self.password_hash = User.hash_to_string(
-      OpenSSL::PKCS5.pbkdf2_hmac(password, password_salt, ITERATIONS, DIGEST.length, DIGEST))
-    end
-  end
-
+  
   def self.hash_to_string(password_hash)
     password_hash.unpack('H*')[0]
   end
@@ -43,5 +33,15 @@ class User < ApplicationRecord
     OpenSSL::PKCS5.pbkdf2_hmac(password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST))
     return user if user.password_hash == hashed_password
     nil
+  end
+
+  private
+
+  def encrypt_password
+    if password.present?
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+      self.password_hash = User.hash_to_string(
+      OpenSSL::PKCS5.pbkdf2_hmac(password, password_salt, ITERATIONS, DIGEST.length, DIGEST))
+    end
   end
 end
