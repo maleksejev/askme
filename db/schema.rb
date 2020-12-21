dfc8b6c9d5e17f51757a93bad118c376acae2aa5
maleksejev <>
2020-12-21 22:05:24 +0200

    index add for username and email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_06_190238) do
+ActiveRecord::Schema.define(version: 2020_12_20_223125) do
 
   create_table "questions", force: :cascade do |t|
     t.string "text"
